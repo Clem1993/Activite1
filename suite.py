efa3a7d1f7c7cac91ee134bfcd0d6d1d672b35e3
clement <laplacecl@hotmail.fr>
2019-05-29 23:26:19 +0200

    calcul de la somme de un

--- suite.py
+++ suite.py
@@ -1,5 +1,10 @@
 u=0
-for i in range(10):
+liste_suite=[u]
+for i in range(1,10):    
     u=u+1
+    print(" a l'odre =",i,"U =",u)
+    liste_suite.append(u)
 
+Somme_liste=sum(liste_suite)
+print(" la somme de la suite a l'ordre",i," est = ",Somme_liste)
 """ suite Un=Un+1"""
